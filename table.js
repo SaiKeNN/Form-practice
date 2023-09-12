16ac004546650c70e47e10e76251416068355677
Kapil Hingu <Kapil.Hingu@ril.com>
2023-09-12 16:55:43 +0530

    first commit

---
+++ table.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("f1");
+    const dataTable = document.getElementById("data-table");
+    const dataContainer = document.querySelector(".container2");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+
+        const firstName = document.getElementById("fName").value;
+        const lastName = document.getElementById("lName").value;
+        const dobInput = document.getElementById("dob1");
+        const email = document.getElementById("email").value;
+        const address = document.getElementById("address").value;
+
+        const currentDate = new Date();
+             
+        const selectedDate = new Date(dobInput.value);
+
+        if (selectedDate > currentDate) {
+            alert("Date of Birth cannot be in the future!");
+            return;
+        }
+
+        const newRow = dataTable.insertRow();
+        newRow.innerHTML = `
+
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${dobInput.value}</td>
+            <td>${email}</td>
+            <td>${address}</td>
+        `;
+
+        form.reset();
+
+        dataContainer.style.display = "block";
+    });
+});
